88e8929a0f975f2af36d34a28ab50efdc848ee17
Deesh Singla <deeshsingla@gmail.com>
2025-07-23 22:48:39 +0530

    fixed cores issue

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,7 +9,7 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173','https://brainly-cvsq.onrender.com'],
     credentials: true,
     exposedHeaders: ['Authorization'],
 }));
